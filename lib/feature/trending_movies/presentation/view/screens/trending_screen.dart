378fc31a5e22afbec6da5ee881c8ed435653b1d7
Hemn Abdulrahman <47257299+hemn-one398@users.noreply.github.com>
2023-09-14 01:31:16 +0300

    creating TrendingListView widget just before creating its bloc

--- lib/feature/trending_movies/presentation/view/screens/trending_screen.dart
+++ lib/feature/trending_movies/presentation/view/screens/trending_screen.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:iq_movies_app/feature/trending_movies/presentation/view/widgets/trending_listview.dart';
 
-class TrendingScreen extends StatelessWidget {
+class TrendingScreen extends StatefulWidget {
   const TrendingScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TrendingScreen> createState() => _TrendingScreenState();
+}
+
+class _TrendingScreenState extends State<TrendingScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('TrendingScreen'),
       ),
-      body: const Center(
-        child: Text('TrendingScreen'),
+      body: const Column(
+        children: [
+          TextField(
+              decoration: InputDecoration(
+            hintText: 'Search by title',
+          )),
+          Expanded(child: TrendingListView()),
+        ],
       ),
     );
   }
