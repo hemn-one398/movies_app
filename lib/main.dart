4be45feb618847d1080bdd767c44ad29cb6ffcd3
Hemn Abdulrahman <47257299+hemn-one398@users.noreply.github.com>
2023-09-14 01:40:04 +0300

    creating MyApp class

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:iq_movies_app/app.dart';
 import 'package:iq_movies_app/feature/trending_movies/data/models/movie.dart';
 
 void main() async {
@@ -7,20 +8,4 @@ void main() async {
   await Hive.initFlutter();
   Hive.registerAdapter(MovieAdapter());
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'IQ Movies',
-        theme: ThemeData(
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-          useMaterial3: true,
-        ),
-        home: const Scaffold());
-  }
 }
