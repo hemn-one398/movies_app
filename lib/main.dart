5d1ca363fec6aaeea22bc1a089fb6f8da88b632c
Hemn Abdulrahman <47257299+hemn-one398@users.noreply.github.com>
2023-09-15 13:25:10 +0300

    adding firebase service

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:iq_movies_app/app.dart';
 import 'package:iq_movies_app/features/trending_movies/data/models/movie.dart';
 import 'package:iq_movies_app/features/trending_movies/data/models/movie_detail.dart';
+import 'package:iq_movies_app/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   await Hive.initFlutter();
   _registerHiveClassAdapters();
   runApp(const MyApp());
@@ -13,7 +18,7 @@ void main() async {
 
 void _registerHiveClassAdapters() {
   Hive.registerAdapter(MovieAdapter());
-    Hive.registerAdapter(MovieDetailAdapter());
+  Hive.registerAdapter(MovieDetailAdapter());
   Hive.registerAdapter(GenreAdapter());
   Hive.registerAdapter(ProductionCompanyAdapter());
   Hive.registerAdapter(ProductionCountryAdapter());
